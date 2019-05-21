4e5f79c83bf7fba354d860f6de1951adce103983
guang.shi <shiguang0116@163.com>
2019-05-21 15:09:36 +0800

    增加colors-console依赖包

--- build/utils.js
+++ build/utils.js
@@ -7,7 +7,7 @@
 
 const glob = require('glob')
 const path = require('path')
-const colors = require('colors')
+const colors = require('colors-console')
 const config = require('./config.js')
 
 const util = {}
@@ -25,7 +25,7 @@ util.getModules = function(globPath) {
         const moduleName = path.basename(url, ext) // 获取文件名
         // 文件名不能重复的验证（moduleName 在这里取的是文件名。建议把所有上级目录的文件夹名称（不包括src/pages）拼接而成的名字作为最终文件名）
         if (modules[moduleName]) {
-            console.error(colors.red(`文件名不能重复使用：${moduleName}。\n`))
+            console.error(colors('red', `文件名不能重复使用：${moduleName}。\n`))
             process.exit(1)
         }
         modules[moduleName] = url
